b1b1edc6a213e0571c62f3dc4279b0095310bc88
wence25 <wencewence25@gmail.com>
2025-04-02 12:29:05 +0200

    src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Theme from './components/Theme.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Theme />
   </StrictMode>,
 )
